f23288f087787a2589e766a5f9fbb43ef958f825
Oleh Deyneka <olde.bitecc@gmail.com>
2022-07-25 08:47:20 +0000

    add jwt helper

---
+++ helper/tokinizer.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET, JWT_REFRESH_SECRET } = require('../configs/config')
+
+function tokinizer(){
+    const access_token = jwt.sign({}, JWT_SECRET, { expiresIn: '10m' })
+    const refresh_token  = jwt.sign({}, JWT_REFRESH_SECRET, { expiresIn: '30d' })
+
+
+    return {
+        access_token,
+        refresh_token
+      }
+}
+
+module.exports = {
+    tokinizer
+}
